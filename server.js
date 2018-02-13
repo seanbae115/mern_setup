d2ad5510a8029d820c26000009a15011e5a0602a
Sean Bae <seanbae115@gmail.com>
2018-02-12 16:19:39 -0800

    Setup server for react deploy

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const express = require('express');
-const PORT = process.env.PORT || 9000;
 const cors = require('cors');
+const path = require('path');
+const PORT = process.env.PORT || 9000;
 
 const app = express();
 
 app.use(cors());
 
-app.get('/', function(req, res){
-    res.send('<h1>App is working with Nodemon</h1>')
-});
+app.use(express.static(path.resolve(__dirname, 'client', 'dist')));
+
+// app.get('/', function(req, res){
+//     res.send('<h1>App is working with Nodemon</h1>')
+// });
 
 app.get('/test', (req, res) => {
     res.send('<h1>This is a test route.</h1>')
@@ -24,6 +27,10 @@ app.get('/api/get-user', (req, res) => {
 
 app.get('/api/get-articles', (req, res) => {
     res.send(['article-1', 'article-2', 'article-3']);
+})
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'));
 })
 
 app.listen(9000, () => {
